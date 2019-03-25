89b22391fad862ffd58f3c9fb6bdd511d70f4bc9
Your Name <you@example.com>
2019-03-25 16:01:47 -0700

    adding servo arm

--- servoArm.py
+++ servoArm.py
@@ -1,48 +1,68 @@
 import RPi.GPIO as GPIO
 import time
+import readchar
 
 # Variables here
-servoPINBase = 17        # This is to control the base movement
-servoPINClaw = 4         # This is to control the claw
-servoPINUD = 27          # This is to control the up and down movement
-servoPINFB = 19            # This is to control the forward and back movement
+servoPINBase = 21        # This is to control the base movement
+servoPINClaw = 4        # This is to control the claw
+servoPINUD = 26          # This is to control the up and down movement
+servoPINFB = 16            # This is to control the forward and back movement
+basePos = 2.5
+stdIncrement = .5
 
-# Funtions here
+leftarm = 6
+rightarm = 6
 def getch():
     inp = raw_input()
     return inp
 
+
 def baseRight():
+    # global basePos
+    # global stdIncrement
+    # basePos += stdIncrement
+    # base.ChangeDutyCycle(basePos)
     base.ChangeDutyCycle(2.5)
     time.sleep(0.5)
-        
+    
 def baseLeft():
-    base.ChangeDutyCycle(12.5)
+    # global basePos
+    # global stdIncrement
+    # basePos -= stdIncrement
+    # base.ChangeDutyCycle(basePos)
+    base.ChangeDutyCycle(12)
     time.sleep(0.5)
     
+
 def pinchClaw():
     claw.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
+    ##time.sleep(0.5)
     
 def openClaw():
     claw.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
+    ##time.sleep(0.5)
     
 def armUp():
-    upDown.ChangeDutyCycle(12.5)    # This goes back
+    upDown.ChangeDutyCycle(10)
+    fowardInitial = 12.5
+    forBack.ChangeDutyCycle(fowardInitial)# This goes back
     time.sleep(0.5)
     
 def armDown():
     upDown.ChangeDutyCycle(2.5)
+    fowardInitial = 12.5
+    forBack.ChangeDutyCycle(12.5)
     time.sleep(0.5)
     
 def armForward():
+    upDown.ChangeDutyCycle(12)
     forBack.ChangeDutyCycle(2.5)    # This goes back
     time.sleep(0.5)
 
 def armBack():
-    forBack.ChangeDutyCycle(12.5)    # This goes back
-    time.sleep(0.5)
+    upDown.ChangeDutyCycle(12)
+    forBack.ChangeDutyCycle(12)    # This goes back
+    time.sleep()
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPINBase, GPIO.OUT)
@@ -66,13 +86,15 @@ forBack.start(2.5)
 while True:
     # This will get the input
     charInput = getch()
+    #charInput = readchar.readkey()
+    #print(repr(readchar.readkey()))
     
     # Left base movement
-    if(charInput == "l"):
+    if(charInput == "a"):
         baseLeft()
         
     # Right base movement
-    if(charInput == "r"):
+    if(charInput == "d"):
         baseRight()
         
     # Pinch claw
@@ -84,11 +106,11 @@ while True:
         openClaw()
         
     # Move arm up
-    if(charInput == "u"):
+    if(charInput == "w"):
         armUp()
     
     # Move arm down
-    if(charInput == "d"):
+    if(charInput == "s"):
         armDown()
         
     # MOve arm forward
